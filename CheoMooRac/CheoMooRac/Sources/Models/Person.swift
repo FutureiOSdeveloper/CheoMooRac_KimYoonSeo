3139043c4073567d1f00fc29bc48701a67404530
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-09-24 13:15:33 +0900

    [#4] dynamic 적용중, 임시 커밋

---
+++ CheoMooRac/CheoMooRac/Sources/Models/Person.swift
@@ -0,0 +1,12 @@
+//
+//  Person.swift
+//  CheoMooRac
+//
+//  Created by 김윤서 on 2021/09/23.
+//
+
+struct Person {
+    var firstName: String
+    var familyName: String
+    var phoneNumber: String
+}
