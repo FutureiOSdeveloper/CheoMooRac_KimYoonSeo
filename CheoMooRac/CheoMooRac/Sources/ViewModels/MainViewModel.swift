c71a365d8cd5d88f5d4eb07671e2f1422fd7277d
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-09-24 14:35:38 +0900

    [#4] Person Model 적용

--- CheoMooRac/CheoMooRac/Sources/ViewModels/MainViewModel.swift
+++ CheoMooRac/CheoMooRac/Sources/ViewModels/MainViewModel.swift
@@ -12,7 +12,7 @@ protocol MainViewModelInput {
 }
 
 protocol MainViewModelOutput {
-    var list:  Dynamic<[String]> {get}
+    var list:  Dynamic<[Person]> {get}
     var sectionHeaderList: Dynamic<[String]> {get}
     var nowRefreshing: Dynamic<Bool> {get}
     var sectionArray: Dynamic<[String]> {get}
@@ -33,7 +33,7 @@ class MainViewModel: MainViewModelProtocol {
     
 
    //  MARK: - OUTPUT
-    let list: Dynamic<[String]> = Dynamic([])
+    let list: Dynamic<[Person]> = Dynamic([])
     let sectionHeaderList: Dynamic<[String]> = Dynamic([])
     let nowRefreshing: Dynamic<Bool> = Dynamic(false)
     let sectionArray: Dynamic<[String]> = Dynamic([])
@@ -41,18 +41,40 @@ class MainViewModel: MainViewModelProtocol {
     func getSectionArray(at section: Int) -> Dynamic<[String]>  {
         
         let list$ = self.list.value.filter {
-            return StringManager.shared.chosungCheck(word: $0) == sectionHeaderList.value[section-1]
+            return StringManager.shared.chosungCheck(word: $0.familyName + $0.firstName) == sectionHeaderList.value[section-1]
+        }.map { person in
+            return person.familyName + person.firstName
         }
+        
         return Dynamic(list$)
     }
     
     
     init() {
-        self.list.value =  ["김윤서", "김루희", "윤예지", "김혜수", "코코", "민재", "잼권이", "리헤이", "노제", "몬익화", "립제이", "잘린이", "엠마", "모아나", "케이데이", "가비", "시미즈zz", "강호동", "이수근", "유재석", "리정" ]
+        self.list.value = [Person(firstName: "윤서", familyName: "김", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "루희", familyName: "김", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "예지", familyName: "윤", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "혜수", familyName: "김", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "리헤이", familyName: "", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "제", familyName: "노", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "엠마", familyName: "", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "모아나", familyName: "", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "민재", familyName: "곽", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "케이데이", familyName: "", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "가비", familyName: "", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "시미즈", familyName: "", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "호동", familyName: "강", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "재석", familyName: "유", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "리정", familyName: "", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "몬익화", familyName: "", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "코코", familyName: "", phoneNumber: "010-6515-6030"),
+                           Person(firstName: "잼권", familyName: "", phoneNumber: "010-6515-6030")
+                           
+        ]
         
         var sectionHeaderList$: [String] = []
-        self.list.value.forEach { name in
-            sectionHeaderList$.append(StringManager.shared.chosungCheck(word: name))
+        self.list.value.forEach { person in
+            sectionHeaderList$.append(StringManager.shared.chosungCheck(word: person.familyName + person.firstName))
         }
         
         self.sectionHeaderList.value = Array(Set(sectionHeaderList$)).sorted()
